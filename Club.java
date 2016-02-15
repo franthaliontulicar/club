28482276b3c85d30e76880d6ad3f26522fbaef4d
paco <franmenendezpouso@gmail.com>
2016-02-15 11:18:16 +0100

    metodo joinedMoth

--- Club.java
+++ Club.java
@@ -16,19 +16,17 @@ public class Club
     {
         // Initialise any fields here ...
         members = new ArrayList<Membership>();
-     
-        
-    }
 
+    }
     /**
      * Add a new member to the club's list of members.
      * @param member The member object to be added.
      */
     public void join(Membership member)
     {
-        
-       members.add(member);
-        
+
+        members.add(member);
+
     }
 
     /**
@@ -40,5 +38,25 @@ public class Club
         int miembros = 0;
         miembros = members.size();
         return miembros;
+    }
+
+    /** 
+     * Calcula el numero de socios que se dieron de alta en un mes determinado. 
+     * El año no nos importa. En caso de que el parametro contenga un valor 
+     * no valido se muestra por pantalla el error.
+     * @param month El mes en el que estamos interesados
+     * @return El numero de socios que se dieron de alta dicho mes
+     */
+
+    public int joinedMonth(int month){
+        String name = "";
+        int year = 0;
+        Membership member = new Membership(name,month,year);
+        for(int numSocios = 0; numSocios < members.size(); numSocios++){
+            numSocios = member.getMonth();
+           
+        }
+        
+         return member.getMonth();
     }
 }
